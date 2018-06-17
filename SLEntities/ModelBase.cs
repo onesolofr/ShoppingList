914c3304bc5b79b9f2e4282099b32781e741dfe0
Onesolo <Onesolo@DESKTOP-QB1LBMK>
2018-06-17 14:30:44 +0200

    Add User

--- SLEntities/ModelBase.cs
+++ SLEntities/ModelBase.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace SLEntities
 {
     public class ModelBase<Identification>
     {
-        Identification Id { get; set; }
+        public Identification Id { get; set; }
+
+        public User CreateBy { get; set; }
+
+        public User ModificationBy { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        [JsonIgnore]
+        public bool IsValid { get; set; }
     }
 }
---
+++ SLEntities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SLEntities
+{
+    public class User : ModelBase<int>
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
